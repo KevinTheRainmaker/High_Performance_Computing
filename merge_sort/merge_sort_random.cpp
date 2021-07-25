6d0dab9f3cf0ce34fb2fdb7b8a7b8ac4b4e28869
KangbeenKo <sweetrainforyou@gmail.com>
2021-07-25 22:44:22 +0900

    Add merge sort

---
+++ merge_sort/merge_sort_random.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <random>
+#include <omp.h>
+
+#define TEST 1  // 1: TEST mode to print out, 0: REAL mode
+
+using namespace std;
+
+void Merge(int* A,int l, int m,int r)
+{
+    int i, j, k;
+    int nL=m-l+1;
+    int nR=r-m;
+    int* L = new int[nL];
+    int* R = new int[nR];
+
+    for(i=0;i<nL;i++) {
+        L[i]=A[l+i];
+    }
+    for(j=0;j<nR;j++) {
+        R[j]=A[m+j+1];
+    }
+
+    i=0,j=0,k=l;
+    while(i<nL && j<nR) {
+        if(L[i]<R[j]) {
+            A[k]=L[i++];
+        }
+        else {
+            A[k]=R[j++];
+        }
+        k++;
+    }
+
+    while(i<nL) {
+        A[k++]=L[i++];
+    }
+
+    while(j<nR) {
+        A[k++]=R[j++];
+    }
+}
+
+void MergeSort(int* A, int l, int r)
+{
+    if (l<r) {
+        int m = (l+r)/2;
+        #pragma omp task shared (A) 
+        MergeSort(A, l, m);
+        #pragma omp task shared (A)
+        MergeSort(A, m+1, r);
+        #pragma omp taskwait
+        Merge(A, l, m, r);
+    }
+}
+
+int main ()
+{
+    // number of array
+    int n;
+    cin >> n;
+    cout << "* n = " <<  n << endl;
+
+    // initialize random seed and generate numbers
+    if (TEST) {
+        srand (time(NULL));
+    }
+    int* numberArray = new int[n];
+    for (int i=0; i<n; i++) {
+        numberArray[i] = rand() % n;
+    }
+
+    if (TEST) {
+        cout << "Before Merge Sorting:" << endl;
+    }
+
+    // call merge sort
+    double begin = omp_get_wtime();
+    #pragma omp parallel
+    {
+        #pragma omp single
+        MergeSort(numberArray, 0, n-1);
+    }
+    double end = omp_get_wtime();
+    cout << "Time: " << end-begin << " (s)" << endl;
+
+    if (TEST) {
+        cout << "After Merge Sorting:" << endl;
+    }
+    delete[] numberArray;
+
+    return 0;
+}
